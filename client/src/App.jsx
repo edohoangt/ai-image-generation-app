5e7d646c77deb9d43374b19e48cb46f915e926ca
Hoang Tran <descience.thh10@gmail.com>
2023-01-25 22:30:17 +0700

    finish ui layout and style

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+
+import { logo } from "./assets";
+import { Home, CreatePost } from "./pages";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-8 border-b border-b-[#e6ebf4]">
+        <Link to="/">
+          <img src={logo} alt="logo" className="w-28 object-contain" />
+        </Link>
+        <Link
+          to="/create-post"
+          className="font-inter font-medium bg-green-800 px-4 py-2 text-white rounded-md"
+        >
+          Create
+        </Link>
+      </header>
+      <main className="sm:p-8 px-4 py-8 w-full bg-[#f9f8fe] min-h-[calc(100vh-73px)]">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/create-post" element={<CreatePost />}></Route>
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
 };
 
 export default App;
